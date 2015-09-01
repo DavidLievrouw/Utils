df192b0a3b7849adcc588698143599ad6690ce04
davidlievrouw <david.lievrouw@gmail.com>
2015-09-01 22:27:47 +0200

    Add some generic utility extensions, add a class overload for IQueryHandler and perform a version bump

--- src/Utils/GenericExtensions.cs
+++ src/Utils/GenericExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace DavidLievrouw.Utils {
@@ -10,7 +11,25 @@ namespace DavidLievrouw.Utils {
     public static IEnumerable<T> ToEnumerable<T>(this T instance) {
       return instance.IsNullOrDefault()
         ? Enumerable.Empty<T>()
-        : new[] { instance };
+        : new[] {instance};
+    }
+
+    public static TResult Get<TObject, TResult>(this TObject @this, Func<TObject, TResult> getter) {
+      return @this != null
+        ? getter(@this)
+        : default(TResult);
+    }
+
+    public static TResult DefaultIf<TResult>(this TResult input, TResult value) {
+      return DefaultIf(input, value, x => x.IsNullOrDefault());
+    }
+
+    public static TResult DefaultIf<TResult>(this TResult input, TResult value, Predicate<TResult> predicate) {
+      if (predicate == null) throw new ArgumentNullException("predicate");
+
+      return predicate(input)
+        ? value
+        : input;
     }
   }
 }
--- src/Utils/IQueryHandler.cs
+++ src/Utils/IQueryHandler.cs
@@ -1,6 +1,10 @@
 ﻿using System.Threading.Tasks;
 
 namespace DavidLievrouw.Utils {
+  public interface IQueryHandler<TResult> {
+    Task<TResult> Handle();
+  }
+
   public interface IQueryHandler<in TArg, TResult> {
     Task<TResult> Handle(TArg request);
   }
