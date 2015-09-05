2cd261264899851259060f7580b58c5fe846d277
davidlievrouw <david.lievrouw@gmail.com>
2015-09-05 15:35:56 +0200

    Add tests for SecretHasher class

---
+++ src/Utils.Tests/Crypto/SecretHasherTests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DavidLievrouw.Utils.Crypto {
+  [TestFixture]
+  public class SecretHasherTests {
+    SecretHasher _sut;
+
+    [SetUp]
+    public void SetUp() {
+      _sut = new SecretHasher();
+    }
+
+    public class CreateHashForSecret : SecretHasherTests {
+      [Test]
+      public void GivenNullSecret_Throws() {
+        Assert.Throws<ArgumentNullException>(() => _sut.CreateHashForSecret(null));
+      }
+
+      [Test]
+      public void GivenEmptySecret_DoesNotThrow_CreatesHash() {
+        var actual = _sut.CreateHashForSecret(string.Empty);
+        Assert.That(actual, Is.Not.Null);
+        Assert.That(actual, Is.Not.Empty);
+      }
+
+      [Test]
+      public void CreatesRandomHashForSecret() {
+        var actual = _sut.CreateHashForSecret("TheSecret");
+        Assert.That(actual, Is.Not.Null);
+        Assert.That(actual, Is.Not.Empty);
+      }
+
+      [Test]
+      public void DoesNotCreateDuplicateHashesForSameSecrets() {
+        var actual1 = _sut.CreateHashForSecret("TheSecret");
+        var actual2 = _sut.CreateHashForSecret("TheSecret");
+        Assert.That(actual1, Is.Not.Null);
+        Assert.That(actual1, Is.Not.Empty);
+        Assert.That(actual2, Is.Not.Null);
+        Assert.That(actual2, Is.Not.Empty);
+        Assert.That(actual1, Is.Not.EqualTo(actual2));
+      }
+    }
+
+    public class ValidateHashForSecret : SecretHasherTests {
+      [Test]
+      public void GivenNullSecret_Throws() {
+        Assert.Throws<ArgumentNullException>(() => _sut.ValidateHashForSecret(null, "somehash"));
+      }
+
+      [Test]
+      public void GivenNullHash_Throws() {
+        Assert.Throws<ArgumentNullException>(() => _sut.ValidateHashForSecret("somesecret", null));
+      }
+
+      [Test]
+      public void GivenGoodHash_ReturnsTrue() {
+        const string secret = "MyDirtyLittleSecret";
+        var goodHash = _sut.CreateHashForSecret(secret);
+        var actual = _sut.ValidateHashForSecret(secret, goodHash);
+        Assert.That(actual, Is.True);
+      }
+
+      [Test]
+      public void GivenBadHash_ReturnsFalse() {
+        const string badHash = "abc123";
+        var actual = _sut.ValidateHashForSecret("TheSecret", badHash);
+        Assert.That(actual, Is.False);
+      }
+    }
+  }
+}
--- src/Utils/Crypto/SecretHasher.cs
+++ src/Utils/Crypto/SecretHasher.cs
@@ -3,15 +3,17 @@ using System.Security.Cryptography;
 
 namespace DavidLievrouw.Utils.Crypto {
   public class SecretHasher : ISecretHasher {
-    const int SaltBytes = 24;
-    const int HashBytes = 24;
-    const int Pbkdf2Iterations = 1000;
+    const int SaltBytes = 24; // Changeable
+    const int HashBytes = 24; // Changeable
+    const int Pbkdf2Iterations = 1000; // Changeable
 
     const int IterationIndex = 1;
     const int SaltIndex = 2;
     const int Pbkdf2Index = 3;
 
     public string CreateHashForSecret(string secret) {
+      if (secret == null) throw new ArgumentNullException(nameof(secret));
+
       var csprng = new RNGCryptoServiceProvider();
       var salt = new byte[SaltBytes];
       csprng.GetBytes(salt);
@@ -23,12 +25,21 @@ namespace DavidLievrouw.Utils.Crypto {
     }
 
     public bool ValidateHashForSecret(string secret, string goodHash) {
+      if (secret == null) throw new ArgumentNullException(nameof(secret));
+      if (goodHash == null) throw new ArgumentNullException(nameof(goodHash));
+
       char[] delimiter = {':'};
       var split = goodHash.Split(delimiter);
-      var iterations = int.Parse(split[IterationIndex]);
-      var salt = Convert.FromBase64String(split[SaltIndex]);
-      var hash = Convert.FromBase64String(split[Pbkdf2Index]);
-
+      var iterations = split.Length <= IterationIndex
+        ? Pbkdf2Iterations
+        : int.Parse(split[IterationIndex]);
+      var salt = split.Length <= SaltIndex
+        ? new byte[] { 0, 0, 0, 0, 0, 0, 0, 0 }
+        : Convert.FromBase64String(split[SaltIndex]);
+      var hash = split.Length <= Pbkdf2Index
+        ? new byte[] { 0 }
+        : Convert.FromBase64String(split[Pbkdf2Index]);
+      
       var testHash = PBKDF2(secret, salt, iterations, hash.Length);
       return SlowEquals(hash, testHash);
     }
