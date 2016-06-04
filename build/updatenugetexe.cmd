1dfd91b3a9d909ada9ff413088f3ad569177e593
David Lievrouw <david.lievrouw@gmail.com>
2016-06-04 19:05:40 +0200

    Update the msbuild projects for the new release

---
+++ build/updatenugetexe.cmd
@@ -0,0 +1,5 @@
+cls
+SET DIR=%~dp0%
+IF NOT EXIST "%DIR%log" MKDIR "%DIR%log"
+"%PROGRAMFILES(X86)%\MSBuild\14.0\Bin\msbuild.exe" /target:UpdateNuGet /v:d "%DIR%/restorepackages.proj" /logger:FileLogger,Microsoft.Build.Engine;LogFile=%DIR%/log/updatenuget.log
+pause
