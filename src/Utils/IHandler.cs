097c741f1aacb25d4be317a060702f3e982998dc
David Lievrouw <david.lievrouw@gmail.com>
2016-12-08 13:53:22 +0100

    Bump to v4.0.0, and bring everything up-to-date.

--- src/Utils/IHandler.cs
+++ src/Utils/IHandler.cs
@@ -1,11 +1,11 @@
 ﻿using System.Threading.Tasks;
 
 namespace DavidLievrouw.Utils {
-  public interface IQueryHandler<TResponse> {
+  public interface IHandler<TResponse> {
     Task<TResponse> Handle();
   }
 
-  public interface IQueryHandler<in TRequest, TResponse> {
+  public interface IHandler<in TRequest, TResponse> {
     Task<TResponse> Handle(TRequest request);
   }
 }
